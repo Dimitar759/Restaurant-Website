525090312c2610e88f48d7a70ffd695e825696f6
Dimitar Nikolov <dimitarnikolov759@gmail.com>
2024-03-07 21:06:33 +0100

    A little Javascript and text added
    Added functionalities to some of the navbar buttons and added text

--- Yummy/script.js
+++ Yummy/script.js
@@ -3,8 +3,12 @@ let reservationsTable = document.getElementById("reservationsTable");
 let bookATableNav = document.getElementById("bookATableNav");
 
 
-// let aboutNavButton= document.getElementById("aboutNav");
-// let aboutUsPage = document.getElementById("aboutUsPage");
+let aboutNavButton= document.getElementById("aboutNav");
+let aboutUsPage = document.getElementById("aboutUsPage");
+
+let learnMoreBtn = document.getElementById("learnMore");
+let menuPage = document.getElementById("menu");
+let menuBtn = document.getElementById("menuBtn");
 
 bookATableButton.addEventListener("click", function(){
     reservationsTable.scrollIntoView({ behavior: 'smooth' });
@@ -14,7 +18,14 @@ bookATableNav.addEventListener("click", function(){
     reservationsTable.scrollIntoView({ behavior: 'smooth' });
 });
 
-// aboutNavButton.addEventListener("click", function(){
-//     aboutUsPage.scrollIntoView({ behavior: 'smooth' });
-// });
+aboutNavButton.addEventListener("click", function(){
+    aboutUsPage.scrollIntoView({ behavior: 'smooth' });
+});
 
+learnMoreBtn.addEventListener("click", function(){
+    aboutUsPage.scrollIntoView({ behavior: 'smooth' });
+});
+
+menuBtn.addEventListener("click", function(){
+    menuPage.scrollIntoView({ behavior: 'smooth' });
+});
